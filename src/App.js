b097b9d611757e0e4ea451da6a8a276f8350945d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-23 09:01:27 +0530

    docs - dynamic value added to anchor tag atribute

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const title = 'welcome to new page';
   const like = 50;
+  const link = "http://www.google.com";
 
   return (
     <div className="App">
@@ -14,7 +15,9 @@ function App() {
         <p>{10}</p>
         <p>{`Hello,sj`}</p>
         <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random()*10}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google site</a>
       </div>
 
     </div>
