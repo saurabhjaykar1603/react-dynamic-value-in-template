9a285960d382c361ef64411e6c53ef83f1f0af73
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-23 08:56:12 +0530

    docs - value added to tag with {}

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 
 function App() {
-  const title = 'welcome to new page'
+  const title = 'welcome to new page';
+  const like = 50;
 
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <p>{like}</p>
+
+        <p>{10}</p>
+        <p>{`Hello,sj`}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random()*10}</p>
       </div>
 
     </div>
